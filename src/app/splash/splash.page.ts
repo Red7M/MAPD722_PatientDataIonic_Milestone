3bc2dae99c8710b97ed3383f0ba601a13c214d48
Red7M <reda.mehali@gmail.com>
2021-03-18 19:41:54 -0400

    navigates between all screens of the app, set UI for first splash screen, and its functionality

--- src/app/splash/splash.page.ts
+++ src/app/splash/splash.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-splash',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class SplashPage {
 
-  constructor() {}
+  constructor(private router: Router) {}
 
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngOnInit() {
+    setTimeout(() => {
+      this.router.navigate(['login']);
+    }, 3000); // 3sec
+  }
 }
